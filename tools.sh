534fa663dccc722e805b959b813539805ea731a6
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-01-17 12:40:49 -0300

    chore: Update java version and add rust tools

--- tools.sh
+++ tools.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git python3-full stow tree maven fzf ripgrep openjdk-23-jdk golang-go -y
+sudo apt install git python3-full stow tree maven fzf ripgrep openjdk-25-jdk golang-go rustup -y
