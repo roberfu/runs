d156bcddde650ccfca037e180a288f72d638821c
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-02-19 18:37:17 -0300

    chore(fedora): Add nvm and fonts config

--- fedora.sh
+++ fedora.sh
@@ -2,7 +2,7 @@
 
 sudo dnf update -y
 sudo dnf install -y neovim python3-neovim mpv alacritty qbittorrent \
-git stow python3 tree maven fzf ripgrep java-25-openjdk golang-go \
+git stow python3 tree maven fzf ripgrep java-21-openjdk golang-go \
 rustup
 
 sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
@@ -16,3 +16,14 @@ gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 metadata_expire=1h
 EOF
 sudo dnf install -y codium
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install node --lts
+source ~/.bashrc
+
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
+cd $HOME/vendors/nerd-fonts
+./install.sh JetBrainsMono
