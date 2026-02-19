a021c7df17f9865f1875cc07363e0033e01bacdb
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-02-19 18:38:44 -0300

    chore(wsl-ubuntu): Add fonts and node config, remove files sh files

--- fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/repos/nerd-fonts
-cd $HOME/repos/nerd-fonts
-./install.sh JetBrainsMono
-
--- wsl-ubuntu.sh
+++ wsl-ubuntu.sh
@@ -2,5 +2,16 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git python3-full stow tree maven fzf ripgrep openjdk-25-jdk \
+sudo apt install -y git python3-full stow tree maven fzf ripgrep openjdk-21-jdk \
 golang-go rustup
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install node --lts
+source ~/.bashrc
+
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
+cd $HOME/vendors/nerd-fonts
+./install.sh JetBrainsMono
