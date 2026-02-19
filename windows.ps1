54e6cf276f1b9f0f2508a7a09e708d771febbfd0
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-02-19 18:37:52 -0300

    chore(windows): Add new workstation

--- windows.ps1
+++ windows.ps1
@@ -1,18 +1,39 @@
 $apps = @(
     "Hibbiki.Chromium",
+    "7zip.7zip",
     "Git.Git",
+    "Neovim.Neovim",
+    "Microsoft.VCRedist.2015+.x64",
     "RevoUninstaller.RevoUninstaller",
     "qBittorrent.qBittorrent",
     "Notepad++.Notepad++",
     "Python.Python.3.14",
-    "CoreyButler.NVMforWindows"
+    "CoreyButler.NVMforWindows",
+    "Oracle.JDK.21",
+    "RedHat.Podman",
+    "ZedIndustries.Zed",
+    "VideoLAN.VLC",
+    "JetBrains.Toolbox",
+    "VSCodium.VSCodium",
+    "Valve.Steam"
 )
+
+function Refresh-Path {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
 
 foreach ($app in $apps) {
     winget install --id $app --silent --accept-package-agreements --accept-source-agreements
 }
 
-$origen = Join-Path $PSScriptRoot "\..\.dotfiles\.gitconfig"
+$origen = Join-Path $PSScriptRoot "\..\dotfiles\.gitconfig"
 $destino = "C:\Users\Roberto\.gitconfig"
 
 Copy-Item -Path $origen -Destination $destino -Force
+
+Refreash-Path
+
+nvm install lts
+nvm use lts
+
+Refresh-Path
