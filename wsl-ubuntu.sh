06678abbdf21cf4565b531536b143865c3c9df76
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-02-23 23:42:59 -0300

    chore(wsl-ubuntu): Update for laptop workstation

--- wsl-ubuntu.sh
+++ wsl-ubuntu.sh
@@ -3,15 +3,19 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git python3-full stow tree maven fzf ripgrep openjdk-21-jdk \
-golang-go rustup
+golang-go rustup neovim
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm install node --lts
-source ~/.bashrc
+source $HOME/.bashrc
 
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
-cd $HOME/vendors/nerd-fonts
-./install.sh JetBrainsMono
+sudo apt-get install ninja-build gettext cmake curl build-essential git
+git clone https://github.com/neovim/neovim $HOME/vendors/neovim
+cd $HOME/vendors/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+source $HOME/.bashrc
