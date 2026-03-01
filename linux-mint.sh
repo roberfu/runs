7dad7a9a690f5819fea7db322cfbb01bed01d0b3
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-03-01 18:29:15 -0300

    chore(linux-mint): Add brave and update java version

--- linux-mint.sh
+++ linux-mint.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y mpv alacritty qbittorrent git python3-full stow tree maven \
-fzf ripgrep openjdk-21-jdk golang-go rustup
+fzf ripgrep openjdk-25-jdk golang-go rustup
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -23,3 +23,5 @@ source $HOME/.bashrc
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
 cd $HOME/vendors/nerd-fonts
 ./install.sh Hack
+
+curl -fsS https://dl.brave.com/install.sh | sh
