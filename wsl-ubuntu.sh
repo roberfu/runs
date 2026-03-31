08cc875cb8235765fc48f5d00818e54876b3ebe4
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-03-31 13:20:25 -0300

    chore(wsl): Add unzip cli install

--- wsl-ubuntu.sh
+++ wsl-ubuntu.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-
 set -e
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git python3-full stow tree maven fzf ripgrep openjdk-21-jdk \
-    golang-go rustup
+sudo apt install -y git python3-full stow tree maven fzf ripgrep openjdk-25-jdk \
+    golang-go rustup unzip
 
 if [ ! -d "$HOME/.nvm" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 fi
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 if ! command -v nvm &> /dev/null; then
-    nvm install node --lts
+    nvm install --lts
 fi
-source $HOME/.bashrc
 
-sudo apt-get install ninja-build gettext cmake curl build-essential git
+sudo apt-get install -y ninja-build gettext cmake curl build-essential git
 
-if ! command -v nvim &> /dev/null; then
+if ! nvim --version &> /dev/null || [[ $(nvim --version 2>/dev/null | head -1 | cut -d' ' -f2) < "v0.9" ]]; then
     if [ ! -d "$HOME/vendors/neovim" ]; then
         git clone https://github.com/neovim/neovim $HOME/vendors/neovim
     fi
-    cd $HOME/vendors/neovim
+    pushd $HOME/vendors/neovim
     git checkout stable
+    git pull
     make CMAKE_BUILD_TYPE=RelWithDebInfo
     sudo make install
+    popd
 fi
-source $HOME/.bashrc
