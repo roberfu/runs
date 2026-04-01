18e7057c52d5b79726f9b79bcb2df3d826c65e7f
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-04-01 02:21:06 -0300

    feat(fedora): Update scripts

--- fedora.sh
+++ fedora.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
-
 set -e
-
 sudo dnf update -y
-sudo dnf install -y neovim python3-neovim mpv alacritty qbittorrent \
-git stow python3 tree maven fzf ripgrep java-21-openjdk golang-go \
-rustup
+sudo dnf install -y mpv alacritty qbittorrent git python3-full stow tree maven \
+    fzf ripgrep java-latest-openjdk golang rustup podman unzip \
+    ninja-build gettext cmake curl gcc gcc-c++ make
+
+if [ ! -d "$HOME/.nvm" ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+fi
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.bashrc
+nvm install --lts
+
+if ! nvim --version &> /dev/null || [[ $(nvim --version 2>/dev/null | head -1 | cut -d' ' -f2) < "v0.9" ]]; then
+    if [ ! -d "$HOME/vendors/neovim" ]; then
+        git clone https://github.com/neovim/neovim $HOME/vendors/neovim
+    fi
+    pushd $HOME/vendors/neovim
+    git checkout stable
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
+    popd
+fi
+
+source ~/.bashrc
+
+npm install -g tree-sitter-cli
+npm i -g opencode-ai
+
+if [ ! -d "$HOME/vendors/nerd-fonts" ]; then
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
+fi
+pushd $HOME/vendors/nerd-fonts
+./install.sh Hack
+popd
 
 if [ ! -f /etc/yum.repos.d/vscodium.repo ]; then
-    sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+    sudo tee /etc/yum.repos.d/vscodium.repo << 'EOF'
 [gitlab.com_paulcarroty_vscodium_repo]
 name=gitlab.com_paulcarroty_vscodium_repo
 baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
@@ -21,15 +51,6 @@ EOF
 fi
 sudo dnf install -y codium
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-nvm install node --lts
-source ~/.bashrc
-
-if [ ! -d "$HOME/vendors/nerd-fonts" ]; then
-    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git $HOME/vendors/nerd-fonts
+if ! command -v zed &> /dev/null; then
+    curl -f https://zed.dev/install.sh | sh
 fi
-cd $HOME/vendors/nerd-fonts
-./install.sh JetBrainsMono
